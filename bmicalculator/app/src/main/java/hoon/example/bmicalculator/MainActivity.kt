b873499bb3a3431a6c1cb86465dc97fde6570ea8
boris920308 <jhoonj0308@gmail.com>
2023-05-22 21:16:10 +0900

    [ feat ] #bmi_calculator 1.결과화면 구성 2.intent.putExtra

--- bmicalculator/app/src/main/java/hoon/example/bmicalculator/MainActivity.kt
+++ bmicalculator/app/src/main/java/hoon/example/bmicalculator/MainActivity.kt
@@ -30,6 +30,9 @@ class MainActivity : AppCompatActivity() {
             Log.d("MainActivity", "height : $height , weight: $weight ")
 
             val intent = Intent(this, ResultActivity::class.java)
+
+            intent.putExtra("height", height)
+            intent.putExtra("weight", weight)
             startActivity(intent)
 
         }
